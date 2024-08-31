ffb9f973cd18bb55d1c77b333772a3c0935acf16
travel15740 <travel15740@gmail.com>
2024-08-31 14:06:26 +0800

    建立 Edit API: patchTodo

--- services/todo-services.js
+++ services/todo-services.js
@@ -82,7 +82,83 @@ const todoServices = {
       })
       .catch(err => cb(err))
   },
-  patchTodo: (req, cb) => cb(null, { messages: '功能開發中' }),
+  patchTodo: (req, cb) => {
+    // 從路由變數拿取 id
+    const todoId = Number(req.params.id)
+
+    // 接住表單傳過來的資料
+    const { name, content, remarks, time, date, location, creator } = req.body
+
+    // 檢查必填資料是否存在或空白
+    if (!name || !name.trim()) {
+      const error = new Error('The todo name is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!content || !content.trim()) {
+      const error = new Error('The todo content is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 將 time 轉換為數值並檢查是否為有效數字
+    const parsedTime = parseFloat(time)
+    if (!time || parsedTime <= 0) {
+      const error = new Error('The expected time for the todo is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!date) {
+      const error = new Error('The date for the todo is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!creator || !creator.trim()) {
+      const error = new Error('The Todo creator is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 操作 model 查詢資料庫
+    return Todo.findByPk(todoId)
+      .then(todo => {
+        // todo = null // 錯誤測試: 預期會出現以下訊息:
+        if (!todo) {
+          const error = new Error('Todo not found!')
+          error.status = 404
+          throw error
+        }
+
+        // 使用 sequelize 方法更新資料
+        return todo.update({
+          name,
+          content,
+          remarks,
+          time: parsedTime,
+          date,
+          location,
+          creator
+        })
+      })
+      .then(editedTodo => {
+        // editedTodo = null // 錯誤測試: 預期會出現以下訊息:
+        if (!editedTodo) {
+          const error = new Error('Todo not found!')
+          error.status = 404
+          throw error
+        }
+
+        const data = { todo: editedTodo }
+        return cb(null, data)
+      })
+      .catch(err => cb(err))
+  },
   deleteTodo: (req, cb) => cb(null, { messages: '功能開發中' })
 }
 
