3253857018b8218ef026e4964f41cfc4af873626
travel15740 <travel15740@gmail.com>
2024-08-31 11:58:16 +0800

    建立 Get API: getTodos

--- services/todo-services.js
+++ services/todo-services.js
@@ -1,8 +1,25 @@
 // 載入所需 model
+const { Todo } = require('../models')
 
 // 設計 todoServices
 const todoServices = {
-  getTodos: (req, cb) => cb(null, { messages: '功能開發中' }),
+  getTodos: (req, cb) => {
+    return Todo.findAll({
+      raw: true
+    })
+      .then(todos => {
+        // todos = null // 錯誤測試: 預期會出現以下訊息:
+        if (!todos) {
+          const error = new Error('Todos not found!')
+          error.status = 404
+          throw error
+        }
+
+        const data = { todos: todos }
+        return cb(null, data)
+      })
+      .catch(err => cb(err))
+  },
   postTodo: (req, cb) => cb(null, { messages: '功能開發中' }),
   patchTodo: (req, cb) => cb(null, { messages: '功能開發中' }),
   deleteTodo: (req, cb) => cb(null, { messages: '功能開發中' })
