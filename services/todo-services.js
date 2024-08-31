10e8492dd4b264744d8bd5635ab9f278dc65fb7d
travel15740 <travel15740@gmail.com>
2024-08-31 12:38:12 +0800

    建立 Add API: postTodo

--- services/todo-services.js
+++ services/todo-services.js
@@ -20,7 +20,68 @@ const todoServices = {
       })
       .catch(err => cb(err))
   },
-  postTodo: (req, cb) => cb(null, { messages: '功能開發中' }),
+  postTodo: (req, cb) => {
+    // 接住表單傳過來的資料
+    const { name, content, remarks, time, date, location, creator } = req.body
+
+    // 檢查必填資料是否存在或空白
+    if (!name || !name.trim()) {
+      const error = new Error('The todo name is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!content || !content.trim()) {
+      const error = new Error('The todo content is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 將 time 轉換為數值並檢查是否為有效數字
+    const parsedTime = parseFloat(time)
+    if (!time || parsedTime <= 0) {
+      const error = new Error('The expected time for the todo is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!date) {
+      const error = new Error('The date for the todo is required!')
+      error.status = 422
+      throw error
+    }
+
+    // 檢查必填資料是否存在或空白
+    if (!creator || !creator.trim()) {
+      const error = new Error('The Todo creator is required!')
+      error.status = 422
+      throw error
+    }
+
+    return Todo.create({
+      name,
+      content,
+      remarks,
+      time: parsedTime,
+      date,
+      location,
+      creator
+    })
+      .then(newTodo => {
+        // newTodo = null // 錯誤測試: 預期會出現以下訊息:
+        if (!newTodo) {
+          const error = new Error('Todo not found!')
+          error.status = 404
+          throw error
+        }
+
+        const data = { todo: newTodo }
+        return cb(null, data)
+      })
+      .catch(err => cb(err))
+  },
   patchTodo: (req, cb) => cb(null, { messages: '功能開發中' }),
   deleteTodo: (req, cb) => cb(null, { messages: '功能開發中' })
 }
